d691e9787639e420a91d86d5e29231262941f59f
Māris Muižnieks <maris.muiznieks@lumi.moe>
2025-04-13 14:18:37 +0300

    Create main.py

---
+++ src/md/no1/main.py
@@ -0,0 +1,28 @@
+def append_to(x, y):
+    try:
+        x.append(int(y))
+    except ValueError:
+        x.append(0)
+        pass
+
+def appendstr_to(x, y):
+    try:
+        x.append(str(y))
+    except ValueError:
+        x.append("NULL")
+
+with open("auto.csv", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+Mark = []
+year = []
+fuel = []
+for l in lines:
+    row = l.rstrip().split(',')
+    if row[1] == "VOLVO":
+        appendstr_to(Mark, row[1])
+        append_to(year, row[6])
+        appendstr_to(fuel ,row[4])
+
+print(f"Latvijā ir reģistrēti {len(Mark)}, {Mark[1]} Markas auto, vecākais ir reģistrēts {max(year)} gadā.")
+
